1f621177b13f2e4e7ed2e56a084312d683b92c38
1219979760@qq.com <1219979760@qq.com>
2020-05-15 14:25:00 +0800

    1

---
+++ Class10-2/Class10-2/源.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include <opencv2\imgproc\types_c.h>
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+	Mat srcMat = imread("C:\\picture\\etest.JPG", 1);
+	Mat dstMat;
+	Mat srcMat1[3];
+
+	Mat channel[3];
+	split(srcMat, channel);
+
+	equalizeHist(channel[0], srcMat1[0]);
+	equalizeHist(channel[1], srcMat1[1]);
+	equalizeHist(channel[2], srcMat1[2]);
+
+	merge(srcMat1,3, dstMat);
+	imshow("img", dstMat);
+	waitKey(0);
+	return 0;
+}
---
+++ Class10-3/Class10-3/源.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include <opencv2\imgproc\types_c.h>
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+	Mat srcMat = imread("C:\\picture\\gtest.JPG", 0);
+	float gamma = 0.45;
+	float Lut[256];
+	for (int i = 0; i < 255; i++)
+	{
+		Lut[i] = pow(i * 1.0 / 255, gamma);
+	}
+	for (int i = 0; i < srcMat.rows; i++)
+	{
+		for (int j = 0; j < srcMat.cols; j++)
+		{
+			srcMat.at<uchar>(i, j) = Lut[srcMat.at<uchar>(i, j)] * 255;
+		}
+	}
+	imshow("img", srcMat);
+	waitKey(0);
+	return 0;
+}
